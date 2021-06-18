28eb471a8c5350498a10990c1ba4b2065d64fa8d
Isa Tekinkaya <isatekinkaya@yahoo.de>
2021-06-18 15:56:25 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 document.getElementById("main").remove()
 let newHeader = document.createElement('h1')
 newHeader.id="victory"
-newHeader.text-content= "YOUR-NAME is the champion inside"
+newHeader.textContent= "YOUR-NAME is the champion inside"
